01a49ccbd9f6797e078edfd561f1baa7a83b3883
IamPekka058 <59747867+IamPekka058@users.noreply.github.com>
2021-03-22 16:25:03 +0100

    Added Example
    Example to show creationTime Attribute

--- src/com/namespace/Main.java
+++ src/com/namespace/Main.java
@@ -1,4 +1,13 @@
 package com.namespace;
 
+import com.namespace.function.Actions;
+
+import java.io.File;
+import java.nio.file.attribute.FileTime;
+
 public class Main {
+    public static void main(String[] args) {
+        //Example
+        Actions.changeAttribute(new File("./test.txt"), "creationTime", FileTime.fromMillis(1426518838000L));
+    }
 }
